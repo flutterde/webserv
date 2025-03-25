d468af532e11b3893977416e6d8a08da8b2d3ead
Abdelaziz Chakkaf <abdelazizchakkaf@Abdelazizs-MacBook-Pro.local>
2025-03-25 21:49:36 +0000

    CGI v1

--- learning/cgi.cpp
+++ learning/cgi.cpp
@@ -1,5 +1,7 @@
 #include "request.hpp"
 #include <unistd.h>
+#include <algorithm>
+
 // SCRIPT_FILENAME – hold the executeble file path
 // REQUEST_METHOD – hold the method name
 // CONTENT_TYPE – e.g., application/json, multipart/form-data
@@ -16,44 +18,104 @@
 
 // void cgi(Request &r)
 
-char ** makeEnv(Request &r, char **mainEnv)
+char **makeEnv(Request &r, char **mainEnv)
 {
 	size_t i = 0;
-	while(mainEnv[i]) ++i;
-	char **env = new char *[i + r.getEnvSize()]; // free this 
+	while (mainEnv[i])
+		++i;
+	char **env = new char *[i + r.getEnvSize()]; // free this
 	i = 0;
-	while(mainEnv[i])
+	while (mainEnv[i])
 	{
 		env[i] = strdup(mainEnv[i]);
 		++i;
 	}
-	for (size_t j = 0; !r.getEnv(j).empty(); ++j) env[i++] = strdup(r.getEnv(j).c_str());
+	for (size_t j = 0; !r.getEnv(j).empty(); ++j)
+		env[i++] = strdup(r.getEnv(j).c_str());
 	env[i] = NULL;
 	return env;
 }
 
-/// @brief find a path of executed command for scripte like usr/bin/python
-char **findPath(Request &r)
+std::vector<std::string> splitPath(char **mainEnv)
 {
-	std::string extantion = r.getPath().substr(r.getPath().find_last_of("."));
+	std::vector<std::string> binPath;
+	for (size_t i = 0; mainEnv[i]; ++i)
+	{
+		std::string tmp = mainEnv[i];
+		if (tmp.compare(0, 5, "PATH=") == 0)
+		{
+			size_t start = 5;
+			size_t end;
+			while (true)
+			{
+				end = tmp.find_first_of(":", start);
+				if (end == std::string::npos)
+					break;
+				binPath.push_back(tmp.substr(start, end - start));
+				start = end + 1;
+			}
+			binPath.push_back(tmp.substr(start));
+			break;
+		}
+	}
+	return binPath;
+}
 
-	// if ()
-
+std::string findExecutablePath(const std::vector<std::string> &binPath, const std::string &executable)
+{
+	for (size_t i = 0; i < binPath.size(); ++i)
+	{
+		std::string path = binPath[i] + "/" + executable;
+		if (access(path.c_str(), X_OK) == 0)
+			return path;
+	}
+	return "";
 }
 
 void cgi(Request &r, char **mainEnv)
 {
 	char **env = makeEnv(r, mainEnv);
-	// std::cout << "---------------- CGI env: ----------------\n";
-	// for (i = 0; env[i]; ++i)
-	// 	std::cout << env[i] << std::endl;
-	char *path;
+	std::vector<std::string> binPath = splitPath(mainEnv);
+	std::string fileExtension;
+	std::string executablePath;
+	size_t t;
+	t = r.getPath().find_last_of(".");
+	if (t != std::string::npos)
+		fileExtension = r.getPath().substr(t);
+	else
+		fileExtension = "";
+	if (fileExtension == ".php")
+		executablePath = findExecutablePath(binPath, "php");
+	else if (fileExtension == ".py")
+		executablePath = findExecutablePath(binPath, "python3");
 
-	// pid_t pid = fork();
-	// if (pid == 0)
-	// {
-
-	// }
-	// else if (pid == -1)
-	// 	exit(EXIT_FAILURE);
+	int pip[2];
+	if (pipe(pip) < 0)
+		std::cerr << "pipe failed\n";
+	pid_t pid = fork();
+	if (pid == 0)
+	{
+		close(pip[0]);
+		dup2(pip[1], STDOUT_FILENO);
+		dup2(pip[1], STDERR_FILENO);
+		close(pip[1]);
+		char *args[3] = {strdup(executablePath.c_str()), strdup(r.getPath().c_str()), NULL};
+		execve(args[0], args, env);
+		std::cerr << "Error: execve failed\n";
+	}
+	else if (pid > 0)
+	{
+		close(pip[1]);
+		char buffer[1024];
+		ssize_t bytesRead;
+		while ((bytesRead = read(pip[0], buffer, sizeof(buffer) - 1)) > 0)
+		{
+			buffer[bytesRead] = '\0';
+			std::cout << buffer;
+		}
+		close(pip[0]);
+		waitpid(pid, NULL, 0);
+	}
+	else
+		exit(EXIT_FAILURE);
 }
